929443bfaee8abecae9fd5d66d8d9f9f4276ef75
om salunke <omsaunke343@gmail.com>
2024-10-24 09:25:04 +0530

    header almost ready

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="home-section">
+    <div
+      className="home-section w-full h-[100vh]"
+      style={{
+        backgroundImage: `url("https://static.wixstatic.com/media/11062b_9b8a1888d9ab40e3aad9390af2ea539e~mv2.jpg/v1/fill/w_293,h_164,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_9b8a1888d9ab40e3aad9390af2ea539e~mv2.jpg)`,
+        backgroundSize: "cover",
+      }}
+    >
       <Navbar />
+      {/* //home section  */}
+
+      <div className="w-full px-8 ">
+        <h1 className="text-white text-6xl pt-8 leading-snug last:leading-0 font-bold w-[50%]">
+          A START-UP FOR START-UPS
+        </h1>
+        <button className="text-black mt-4 bg-white flex gap-4 flex-row py-2 rounded-full items-center justify-between w-[35vh] px-2">
+          <span className="ml-4">Get Started</span>
+          <span>
+            <img
+              width="40"
+              height="40"
+              src="https://img.icons8.com/ios-filled/50/circled-chevron-right.png"
+              alt="circled-chevron-right"
+            />
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,52 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import "@/app/globals.css";
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[12vh] flex flex-row items-center justify-between px-8 bg-gray-900 text-white'>
-      <div className='flex gap-8 '>
-        <span className='text-xl'><strong>EQVISOR</strong></span>
-        <ul className='flex gap-6'>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Team</li>
-            <li>Contact</li>
+    <div className=" sticky top-0 left-0 w-full h-[12vh] flex flex-row items-center justify-between px-8 bg-black">
+      <div className="flex gap-16">
+        <span className="text-[1.4rem] text-white">
+          <strong>EQVISOR</strong>
+        </span>
+        <ul className="flex gap-8 items-center">
+          <li>
+            <Link href="/">
+              <span className="hover:text-blue-300 text-white cursor-pointer">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/services">
+              <span className="hover:text-blue-300 text-white cursor-pointer">
+                Services
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/team">
+              <span className="hover:text-blue-300 text-white cursor-pointer">
+                Team
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <span className="hover:text-blue-300 text-white cursor-pointer">
+                Contact
+              </span>
+            </Link>
+          </li>
         </ul>
       </div>
       <div>
-        <button className='bg-white text-black py-[10px] px-6 rounded-full'>Contact</button>
+        <button className="bg-white hover:bg-purple-300 text-black py-[10px] px-6 rounded-full">
+          Contact
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
