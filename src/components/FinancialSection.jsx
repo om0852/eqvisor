901b6b0f97d0aa34dd05dcc7009b21bb565f7f6f
om salunke <omsaunke343@gmail.com>
2024-10-24 16:37:34 +0530

    financialSection completed

--- src/components/FinancialSection.jsx
+++ src/components/FinancialSection.jsx
@@ -22,14 +22,14 @@ const FinancialSection = () => {
 
   return (
     <div className="w-full h-[auto] top-rounded bg-white top-[-5vh]">
-      <div className="flex sticky pt-[20vh]">
-        <div className="w-[50%] h-[50vh] pl-[50px] flex flex-col justify-between">
-          <div className="sticky top-0">
+      <div className="flex  pt-[20vh]">
+        <div className="w-[50%] sticky top-[20vh] h-[60vh] pl-[50px] flex flex-col justify-between">
+          <div className="">
             <h1 className="text-xl font-semibold">FINANCIAL TOOLS</h1>
             <p className="text-[2.7rem] pt-2">Streamline Your Start-up</p>
           </div>
-          <div className="sticky top-[50vh]">
-            <p className="">
+          <div className="">
+            <p className="w-[90%] text-xl">
               This is the space to introduce the Services section. Briefly
               describe the types of services offered and highlight any special
               benefits or features.
@@ -40,11 +40,14 @@ const FinancialSection = () => {
           <ul id="cards" className="text-black">
             {cardData.map((data, index) => (
               <li key={index} className="card" id={`card${index + 1}`}>
-                <div className="card-body flex flex-col justify-between py-[20px] px-20 ">
+                <div className="card-body flex flex-col justify-between py-[20px] px-20 " style={{
+                    background:(index%2==0)?"rgb(23,23,23)":"rgb(56,74,211)",
+                    color:(index%2==0)?"white":"black"
+                }}>
                   <h1 className="text-4xl">0{index + 1}</h1>
                   <div>
-                    <h2>{data.title}</h2>
-                    <p>{data.description}</p>
+                    <h2 className="text-4xl font-semibold py-4">{data.title}</h2>
+                    <p className="text-3xl">{data.description}</p>
                   </div>
                 </div>
               </li>
