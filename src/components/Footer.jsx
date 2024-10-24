f97e0f09ce603eabfaf7ce500adc751ff3d77dfb
om salunke <omsaunke343@gmail.com>
2024-10-24 23:11:40 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,55 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Footer = () => {
+  const [formStatus, setFormStatus] = useState({ email: false, sub: false });
+
+  const handleSubmit = (event) => {
+    // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const email = formData.get("email");
+    const sub = formData.get("sub");
+    console.log(email, sub);
+
+    if (!email) {
+      setFormStatus((prev) => ({ email: true, sub: prev.sub }));
+    }
+    if (!sub) {
+      setFormStatus((prev) => ({ email: prev.email, sub: true }));
+    }
+    if (email) {
+      setFormStatus((prev) => ({ email: false, sub: prev.sub }));
+    }
+    if (sub) {
+      setFormStatus((prev) => ({ email: prev.email, sub: false }));
+    }
+  };
+
+  const data = [
+    {
+      section: "QUICK LINKS",
+      links: ["Home", "About", "Solutions", "Contact"],
+    },
+    {
+      section: "SOCIAL",
+      links: ["Linkedin", "Github", "Instagram"],
+    },
+    {
+      section: "CONTACT",
+      links: ["eqvisor@gmail.com", "9307154076", "Address"],
+    },
+    {
+      section: "POLICY",
+      links: [
+        "Terms & Conditions",
+        "Privacy Policy",
+        "Accessibility Statement",
+      ],
+    },
+  ];
+
   return (
     <>
       <div className="footer-container">
@@ -42,12 +91,129 @@ const Footer = () => {
                 color: "white",
                 borderRadius: "3vh 3vh 0 0",
               }}
-              className="footer-card-body flex flex-col justify-between py-[20px] px-20 bg-blue-600"
+              className="footer-card-body flex flex-col justify-between py-[20px] px-10 bg-blue-600"
             >
               <h1 className="text-[6rem] font-bold py-[6vh]">EQVISOR</h1>
-              <div>
+              <div className="flex flex-row justify-start gap-0">
                 <div>
                   <p>JOIN OUR NEWSLETTER</p>
+                  <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col gap-2 py-6 w-[55vh]"
+                  >
+                    <label htmlFor="email">Email</label>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="eqvisor@gmail.com"
+                      className="outline-none bg-transparent border-b-2"
+                    />
+                    <p>
+                      {formStatus.email ? (
+                        <div className="flex items-center gap-2 text-red-500">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            x="0px"
+                            y="0px"
+                            width="20"
+                            height="20"
+                            viewBox="0,0,256,256"
+                          >
+                            <g
+                              fill="#f70a0a"
+                              fillRule="nonzero"
+                              stroke="none"
+                              strokeWidth="1"
+                              strokeLinecap="butt"
+                              strokeLinejoin="miter"
+                              strokeMiterlimit="10"
+                              strokeDasharray=""
+                              strokeDashoffset="0"
+                              fontFamily="none"
+                              fontWeight="none"
+                              fontSize="none"
+                              textAnchor="none"
+                              style={{ mixBlendMode: " normal" }}
+                            >
+                              <g transform="scale(8.53333,8.53333)">
+                                <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.627 -5.373,-12 -12,-12zM16,21h-2v-7h2zM15,11.5c-0.828,0 -1.5,-0.672 -1.5,-1.5c0,-0.828 0.672,-1.5 1.5,-1.5c0.828,0 1.5,0.672 1.5,1.5c0,0.828 -0.672,1.5 -1.5,1.5z"></path>
+                              </g>
+                            </g>
+                          </svg>{" "}
+                          <p> Invalid Email</p>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </p>
+                    <div className="flex justify-evenly items-center py-4">
+                      <span>
+                        <span className="flex flex-row items-start">
+                          <input
+                            className="mr-2 mt-1 outline-none bg-transparent border-[1px] border-white"
+                            type="checkbox"
+                            id="sub"
+                            name="sub"
+                          />
+                          <label htmlFor="sub">
+                            <p> Yes, subscribe me to your newsletter</p>
+                          </label>
+                        </span>
+                        {formStatus.sub ? (
+                          <div className="flex items-center gap-2 text-red-500 ">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              x="0px"
+                              y="0px"
+                              width="20"
+                              height="20"
+                              viewBox="0,0,256,256"
+                            >
+                              <g
+                                fill="#f70a0a"
+                                fillRule="nonzero"
+                                stroke="none"
+                                strokeWidth="1"
+                                strokeLinecap="butt"
+                                strokeLinejoin="miter"
+                                strokeMiterlimit="10"
+                                strokeDasharray=""
+                                strokeDashoffset="0"
+                                fontFamily="none"
+                                fontWeight="none"
+                                fontSize="none"
+                                textAnchor="none"
+                                style={{ mixBlendMode: " normal" }}
+                              >
+                                <g transform="scale(8.53333,8.53333)">
+                                  <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.627 -5.373,-12 -12,-12zM16,21h-2v-7h2zM15,11.5c-0.828,0 -1.5,-0.672 -1.5,-1.5c0,-0.828 0.672,-1.5 1.5,-1.5c0.828,0 1.5,0.672 1.5,1.5c0,0.828 -0.672,1.5 -1.5,1.5z"></path>
+                                </g>
+                              </g>
+                            </svg>{" "}
+                            <p> Check the box to continue</p>
+                          </div>
+                        ) : (
+                          ""
+                        )}
+                      </span>
+                      <button className="py-2 px-6 hover:bg-purple-400 bg-white text-black rounded-full">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div className="flex px-[15vh] flex-row justify-between w-full">
+                  {data.map((data1, index) => (
+                    <div key={index}>
+                      <p>{data1.section}</p>
+                      <ul>
+                        {data1?.links?.map((link, index2) => (
+                          <li key={index2}>{link}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
