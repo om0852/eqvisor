cd3fbb847f863c021e4d27582de9c7341c6e21a8
om salunke <omsaunke343@gmail.com>
2024-10-25 13:56:02 +0530

    always connected done

--- src/components/QuerySection.jsx
+++ src/components/QuerySection.jsx
@@ -23,23 +23,23 @@ const QuerySection = () => {
   ];
 
   return (
-    <div className="w-full bg-stone-300 h-auto py-[10vh] px-[10vh] overflow-hidden">
-      <div className="py-[10vh] h-[70vh]">
-        <h1 className="uppercase text-2xl">Why Eqvisor?</h1>
-        <p className="w-[80vh] text-5xl">Your Satisfaction is our priority</p>
+    <div className="w-full bg-stone-300 h-auto py-[40px] text-black md:py-[10vh] px-[30px] md:px-[10vh] overflow-hidden">
+      <div className="py-[10vh] h-[30vh] md:h-[70vh] text-black">
+        <h1 className="uppercase text-xl md:text-2xl">Why Eqvisor?</h1>
+        <p className="w-[80%] text-3xl md:text-5xl">Your Satisfaction is our priority</p>
       </div>
-      <div className="w-full h-[50vh]">
-        <p className="max-w-[50%] text-2xl">
+      <div className="w-full sm:py-[2vh] md:h-[50vh] text-black">
+        <p className="w-[100%] md:max-w-[50%] text-xl md:text-2xl">
           This is the space to introduce the Services section. Briefly describe
           the types of services offered and highlight any special benefits or
           features.
         </p>
       </div>
 
-      <div className="w-full max-w-[50%] ">
+      <div className="w-full md:max-w-[50%] ">
         {servicesData.map((data, index) => (
-          <div key={index} className="flex flex-col gap-4 py-[5vh]">
-            <div className="w-full flex gap-4 text-3xl text-blue-700">
+          <div key={index} className="flex flex-col gap-3 md:gap-4 py-[5vh]">
+            <div className="w-full flex gap-4 text-xl md:text-3xl text-blue-700">
               <span>{data.index}</span>
               <span>{data.question}</span>
             </div>
