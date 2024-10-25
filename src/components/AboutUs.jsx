50ea2beaf4eda3e642b274ee038db15f3de819b0
om salunke <omsaunke343@gmail.com>
2024-10-25 13:49:31 +0530

    about section done

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,23 +2,21 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="w-full h-[auto] overflow-hidden">
-      <div className="py-[20vh] px-[20vh]">
-        <h1 className="text-2xl font-semibold">About Us</h1>
-        <p className="text-5xl w-[80%]">
+    <div className="w-full h-[auto] overflow-hidden bg-white text-black">
+      <div className="py-[20vh] pl-[30px]  w-full md:px-[20vh]">
+        <h1 className=" text-xl md:text-2xl font-semibold">About Us</h1>
+        <p className=" text-2xl md:text-5xl w-[95%] md:w-[80%]">
           This is a space to share more about the business: who's behind it,
           what it does and what this site has to offer. It’s an opportunity to
           tell the story behind the business or describe a special service or
           product it offers.
         </p>
-        <button className="text-white mt-4 bg-blue-600 hover:bg-black flex gap-4 flex-row py-2 rounded-full items-center justify-between w-[35vh] px-2">
+        <button className="text-white mt-4 bg-blue-600 hover:bg-black flex gap-4 flex-row py-2 rounded-full items-center justify-between w-[20vh] md:w-[35vh] px-2">
           <span className="ml-4">Read More</span>
           <span className="rounded-full p-2 bg-white">
             <img
-              width="20"
-              height="20"
               src="https://img.icons8.com/?size=50&id=15195&format=png&color=111111"
-              className="text-white"
+              className="text-white md:w-[20px] md:h-[20px] w-[14px] h-[14px] "
               alt="circled-chevron-right"
             />
           </span>
