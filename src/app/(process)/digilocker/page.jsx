e9ce0cc66a31fb02fed85bc230194c53c42c8932
om salunke <omsaunke343@gmail.com>
2024-11-03 21:47:21 +0530

    digilocker process page done

---
+++ src/app/(process)/digilocker/page.jsx
@@ -0,0 +1,32 @@
+import StepComponent from "@/components/StepComponent";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <div className="relative z-[5] w-[100%] md:w-[70%] h-[70%]">
+        <StepComponent
+          step={1}
+          totalStep={7}
+          description={"Aadhaar KYC using DigiLocker"}
+        />
+        <div className="relative bg-white z-[5]  rounded-xl flex flex-col gap-10 w-[70%] pt-[3vh] pb-[5vh] h-[70%]">
+          <p>
+            We will collect your address-related information from DigiLocker
+            account for KYC. Your Aadhaar number/copy will not be stored.
+            <br></br>
+            <br></br>
+            <br></br>
+            The Aadhaar and PAN ABCD0789I should belong to you, the applicant.
+            If it does not,<Link className="text-blue-500" href={"/panprocess"}>start over</Link>
+          </p>
+          <button className="w-full rounded-md bg-blue-600 py-2 text-white">Continue to DigiLocker</button>
+<p className="text-slate-400 text-sm pt-[6vh]">*By clicking the above button, I consent to use my Aadhaar related documents and information to conduct online KYC for opening an account in the EQvisor pvt. ltd. in order to avail the services they provide.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/(process)/panprocess/page.jsx
+++ src/app/(process)/panprocess/page.jsx
@@ -1,37 +1,68 @@
+"use client";
+import React, { useState } from "react";
 import Input from "@/components/Input";
 import StepComponent from "@/components/StepComponent";
 import Link from "next/link";
-import React from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const PanProcess = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [pan, setPan] = useState("");
+  const [dob, setDob] = useState("");
+
+  const handleContinue = () => {
+    if (!name || !pan || !dob) {
+      toast.error("Please fill out all fields.");
+      return;
+    }
+    router.push("digilocker");
+  };
+
+  // Set max date to today’s date
+  const today = new Date().toISOString().split("T")[0];
+  console.log(today);
+
   return (
     <div className="w-full h-[100vh] flex items-center justify-center">
-      <div className="w-[70%] h-[70%]">
+      <div className="absolute top-[-3vh] right-[-70vh] w-[100vh] md:w-[200vh] h-[100vh] bg-blue-400 rounded-bl-full "></div>
+      <div className="relative z-[5] w-[100%] md:w-[70%] h-[70%]">
         <StepComponent
           step={1}
           totalStep={7}
           description={"Starting with your PAN"}
         />
-        <div className="login-shadow flex flex-col gap-10 w-[70%] p-[10vh] pb-[5vh] h-[70%]">
+        <div className="relative bg-white z-[5] login-shadow rounded-xl flex flex-col gap-10 w-[70%] p-[10vh] pb-[5vh] h-[70%]">
           <input
             type="text"
-            className="w-[100%] outline-none border-b-2 border-gray-600"
+            className="z-5 w-[100%] outline-none border-b-2 border-gray-600"
             placeholder="Enter name as per PAN"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <div className="w-full flex items-center py-[2vh] gap-8">
             <input
               type="text"
-              className="w-[90%] outline-none border-b-2 border-gray-600"
+              className="z-5 w-[90%] outline-none border-b-2 border-gray-600"
               placeholder="Enter PAN"
+              value={pan}
+              onChange={(e) => setPan(e.target.value)}
             />
             <input
               type="date"
-              className=" w-[90%] outline-none border-b-2 border-gray-600"
+              className="z-5 w-[90%] outline-none border-b-2 border-gray-600"
               placeholder="DOB"
+              max={today}
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
             />
           </div>
           <div className="w-full h-full flex items-end justify-end text-bottom">
-            <Link className="text-blue-500 flex gap-2 items-center" href="/">
+            <p
+              className="text-blue-500 flex gap-2 items-center cursor-pointer"
+              onClick={handleContinue}
+            >
               Continue
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +78,7 @@ const PanProcess = () => {
                   d="m8.25 4.5 7.5 7.5-7.5 7.5"
                 />
               </svg>
-            </Link>
+            </p>
           </div>
         </div>
       </div>
