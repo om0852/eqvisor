9ba38a20ed0ecacf77d931caa5096ae604ca63f5
om salunke <omsaunke343@gmail.com>
2024-11-04 10:53:40 +0530

    digilocker allow and deny page done

---
+++ src/app/(process)/digilocker/_components/DigiLockerAccept.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const DigiLockerAccept = () => {
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center overflow-hidden">
+      <div className="relative flex justify-center px-[1vh] z-[5] w-[100%] md:w-[70%] h-[70%]">
+        <div className="login-shadow w-[100%] h-full flex gap-6 flex-col justify-center items-center md:py-[10vh] px-[4vh] md:px-[20vh]">
+          <div className="flex flex-row items-center  justify-center gap-6 w-[100%] md:w-[70%]">
+            <img src="/eqvisor_logo_2.png" className="w-[10vh] md:w-[25vh]" />
+            <div className="w-[1px] bg-gray-400 h-[5vh] md:h-[10vh]"></div>
+            <img src="/digilocker_logo.png" className="w-[8vh] h-[5vh] md:w-[15vh] md:h-[10vh]" />
+          </div>
+          <div className="text-center text-md md:text-xl">
+            You are providing your consent to share your DigiLocker document
+            with EQvisor.
+          </div>
+          <div className=" cursor-pointer flex w-full overflow-hidden rounded-full border-[1px] border-black ">
+            <div className="flex gap-2 justify-center w-[50%] text-center bg-blue-500 text-white py-[1vh]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="white"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              Allow
+            </div>
+            <div className="w-[50%] text-center  py-[1vh]">Deny</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DigiLockerAccept;
--- src/app/(process)/digilocker/page.jsx
+++ src/app/(process)/digilocker/page.jsx
@@ -1,8 +1,14 @@
+"use client";
 import StepComponent from "@/components/StepComponent";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import DigiLockerAccept from "./_components/DigiLockerAccept";
 
 const Page = () => {
+  const [allowPage, setAllowPage] = useState(true);
+  if (allowPage) {
+    return <DigiLockerAccept />;
+  }
   return (
     <div className="w-full h-[100vh] flex items-center justify-center">
       <div className="relative z-[5] w-[100%] md:w-[70%] h-[70%]">
@@ -19,10 +25,20 @@ const Page = () => {
             <br></br>
             <br></br>
             The Aadhaar and PAN ABCD0789I should belong to you, the applicant.
-            If it does not,<Link className="text-blue-500" href={"/panprocess"}>start over</Link>
+            If it does not,
+            <Link className="text-blue-500" href={"/panprocess"}>
+              start over
+            </Link>
           </p>
-          <button className="w-full rounded-md bg-blue-600 py-2 text-white">Continue to DigiLocker</button>
-<p className="text-slate-400 text-sm pt-[6vh]">*By clicking the above button, I consent to use my Aadhaar related documents and information to conduct online KYC for opening an account in the EQvisor pvt. ltd. in order to avail the services they provide.</p>
+          <button className="w-full rounded-md bg-blue-600 py-2 text-white">
+            Continue to DigiLocker
+          </button>
+          <p className="text-slate-400 text-sm pt-[6vh]">
+            *By clicking the above button, I consent to use my Aadhaar related
+            documents and information to conduct online KYC for opening an
+            account in the EQvisor pvt. ltd. in order to avail the services they
+            provide.
+          </p>
         </div>
       </div>
     </div>
